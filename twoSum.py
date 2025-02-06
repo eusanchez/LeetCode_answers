e78c2d19e6bd661ab4b8adef574cbd1e3188f899
Sanchez Villalobos, Ana <ana.sanchez.villalobos@intel.com>
2025-02-06 17:27:16 -0600

    Removing prints for debugging.

--- twoSum.py
+++ twoSum.py
@@ -1,1 +1,11 @@
 #Two Sum Excercise. 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # num is expected to be List[int]
+        # -> List[int] this means that the function returns a list of integers.
+        size = len(nums)
+        for i in range(size-1):
+            for j in range(i + 1, size):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
