b9666f1be887b31da3cc73915e92f83ca57111a9
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2025-10-09 20:26:34 -0500

    Update ValidPalindrome.py

--- ValidPalindrome.py
+++ ValidPalindrome.py
@@ -1,1 +1,11 @@
-valid palindrome 
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = ''.join(e for e in s if e.isalnum()).lower()
+        if s == s[::-1]:
+            return True
+        else:
+            return False 
