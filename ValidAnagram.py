9a822831ff8180d411e64366d2a113bb25438d08
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2025-10-14 20:53:12 -0500

    Update ValidAnagram.py

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,1 +1,28 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "rat", t = "car"
+
+# Output: false
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        list1 = [char for char in s]
+        list1.sort()
+        list2 = [char for char in t]
+        list2.sort()
+        return (list1 == list2)
