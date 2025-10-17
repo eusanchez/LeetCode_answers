28bbee39ec6d723a5437da048da264f212434ebb
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2025-10-17 11:17:20 -0500

    Add function to group anagrams in a list
    Implemented a function to group anagrams from a list of strings using a defaultdict.

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,1 +1,41 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
+ 
+
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+# Explanation:
+
+# There is no string in strs that can be rearranged to form "bat".
+# The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+# The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+# Example 2:
+
+# Input: strs = [""]
+
+# Output: [[""]]
+
+# Example 3:
+
+# Input: strs = ["a"]
+
+# Output: [["a"]]
+
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        grouped_anagrams = defaultdict(list)
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            grouped_anagrams[sorted_word].append(word)
+        return list(grouped_anagrams.values())
