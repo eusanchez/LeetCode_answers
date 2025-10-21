3a1d704399b398ddcee078991d38bb56040cd788
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2025-10-21 18:38:02 -0500

    Add topKFrequent method to Solution class NOT FINISHED DUE MIDTERM
    Implement topKFrequent method to find elements occurring at least k times.

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -1,1 +1,17 @@
 
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        result = dict((i, nums.count(i)) for i in nums)
+        lista = []
+        for key , value in result.iteritems():
+            if value >= k: 
+                lista.append(key)
+                print(lista)
+            print(lista)
+        print(lista)        
+        return lista
