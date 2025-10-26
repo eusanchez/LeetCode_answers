329912bfe3272cab48093a9dcae9c9dd0091e9f0
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2025-10-26 11:29:28 -0500

    Optimize topKFrequent method implementation
    Refactor topKFrequent method to use counting and bucket sorting for improved efficiency.

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -1,4 +1,21 @@
 
+#class Solution(object):
+    #def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        #result = dict((i, nums.count(i)) for i in nums)
+        #lista = []
+        #for key , value in result.iteritems():
+            #if value >= k: 
+                #lista.append(key)
+                #print(lista)
+            #print(lista)
+        #print(lista)        
+        #return lista
+
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -6,12 +23,18 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        result = dict((i, nums.count(i)) for i in nums)
-        lista = []
-        for key , value in result.iteritems():
-            if value >= k: 
-                lista.append(key)
-                print(lista)
-            print(lista)
-        print(lista)        
-        return lista
+        freq = [[] for i in range(len(nums) +1)]
+        count = {}
+
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+        for n,c in count.items():
+            freq[c].append(n)
+
+
+        answer = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                answer.append(n)
+                if len(answer) == k:
+                    return answer
