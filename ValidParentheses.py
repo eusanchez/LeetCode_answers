675ef104defcbb69e062b5360d792e792a7fc48b
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2025-11-03 11:31:09 -0600

    Add isValid method to check valid parentheses NOT DONE
    Implement a solution to validate parentheses in a string.

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,1 +1,31 @@
-
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        index = 0
+        for item in s:
+            stack.append(item)
+            print(len(stack))
+        while index < len(stack):
+            print(stack[index-1])
+            if (stack[index] == "("):
+                index += 1
+                if(stack[index] == ")"):
+                    return True
+                else:
+                    return False
+            elif (stack[index] == "["):
+                index += 1
+                if (stack[index] == "]"):
+                    return True
+                else:
+                    return False
+            elif (stack[index] == "{"):
+                index += 1
+                if (stack[index] == "}"):
+                    return True
+                else:
+                    return False
