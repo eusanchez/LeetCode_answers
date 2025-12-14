254b38108602a8126c60a08f226196b432899391
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2025-12-13 18:29:51 -0600

    Clean up RemoveDuplicateFromSortedList.py
    Remove incomplete code and comments from the file.

--- RemoveDuplicateFromSortedList.py
+++ RemoveDuplicateFromSortedList.py
@@ -1,5 +1,3 @@
--- INCOMPLETE -- 
-
 # Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 
 # Definition for singly-linked list.
