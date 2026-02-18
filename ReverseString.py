3582f392d995e447bce178c90237ca917a23a9ee
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2026-02-18 18:02:14 +0000

    modify reverse string without reverse

--- ReverseString.py
+++ ReverseString.py
@@ -20,3 +20,22 @@ class Solution(object):
         :rtype: None Do not return anything, modify s in-place instead.
         """
         return s.reverse()
+
+''' Reverse String without .reverse()'''
+
+'''
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+
+        left = 0
+        right = len(s)-1
+
+        while left < right:
+            s[left], s[right] = s[right] = s[left]
+            left += 1
+            right -= 1
+'''
