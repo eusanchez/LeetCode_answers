b62313c945d88c17be6dcf6f9934ab20567dbbe4
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2026-02-18 17:42:20 +0000

    other solution

--- Fibonacci_number.py
+++ Fibonacci_number.py
@@ -44,4 +44,29 @@ class Solution(object):
                 curr = prev + curr
                 prev = temp
             return temp
+
+## ANOTHER SOLUTION OPTION
+
+'''
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        prev = 0
+        curr = 1
+        temp = 0
+
+        if(n == 1):
+            return 1
+        elif (n == 0):
+            return 0
+        else: 
+            for i in range(n-1):
+                temp = curr
+                curr = prev + curr
+                prev = temp
+            return curr
+'''
             
