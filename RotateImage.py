df658c465c647d7931845247820c04bc329b6727
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2026-02-25 04:04:07 +0000

    managing

--- RotateImage.py
+++ RotateImage.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        column = len(matrix[0])
-        row = len(matrix)
-        result = []
+        
+        #column = len(matrix[0])
+        #row = len(matrix)
+        #result = []
 
         #col = [matrix[i][1] for i in range(len(matrix))]
 
@@ -16,6 +17,17 @@ class Solution(object):
                 col.append(matrix[i][j])
             result.append(col[::-1])'''
 
-  
-        
-        
+        n = len(matrix)
+
+        # Step 1: Transpose (swap rows and columns)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        print(matrix)
+
+        # Step 2: Reverse each row
+        for i in range(n):
+            #matrix[i] = complete row
+            #matrix[i][::-1] = complete row flipper over. 
+            matrix[i] = matrix[i][::-1]
